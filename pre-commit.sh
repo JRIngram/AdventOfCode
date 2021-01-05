4f4cbc6167c10ceb3c10ef9241e10601486af4ef
Jamie Ingram <jr-ingram@hotmail.co.uk>
2021-01-05 18:36:57 +0000

    Add doc string to pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Add the following the code to your pre-push hook file
+# MAIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# $MAIN_DIR/../../pre-commit.sh
+
 CWD=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/2020/adventofcode2020/
