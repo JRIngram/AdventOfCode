2934dcd2376c11a713cd38137ac493e8fba140b1
Jamie Ingram <jr-ingram@hotmail.co.uk>
2024-03-09 22:05:16 +0000

    Add day seven solution

--- 2023/days/seven.py
+++ 2023/days/seven.py
@@ -1,5 +1,6 @@
 from timeit import default_timer
 from utils import read_file
+import re
 from functools import cmp_to_key
 from enum import Enum
 
@@ -16,7 +17,7 @@ def seven(scenario: int, input_path: str):
             "bid": int(split_line[1])
         }
         card_bids.append(card_bid)
-    sorted_bid_list = quicksort_bids(card_bids)
+    sorted_bid_list = quicksort_bids(card_bids, scenario)
     for index, card_bid in enumerate(sorted_bid_list):
         solution = solution + (card_bid.get("bid") * (index + 1))
 
@@ -35,27 +36,48 @@ class HandType(Enum):
     ONE_PAIR = 1
     HIGH_CARD = 0
 
-def get_hand_type(hand: str) -> HandType:
-    split_hand = list(hand)
-    card_set = set(split_hand)
-    occurences = dict.fromkeys(card_set, 0)
-    for card_key in card_set:
-        card_occurences = 0
-        for card in split_hand:
-            if card == card_key:
-                card_occurences = card_occurences + 1
-        occurences.update({ card_key: card_occurences})
+def get_alpha_card_values(card, scenario: int):
+    alpha_card_regex = '[TJQKA]'
+    alpha_card_values_scenario_one = {
+        "T": 10,
+        "Q": 12,
+        "K": 13,
+        "A": 14,
+    }
+    if re.match(alpha_card_regex, card):
+        if card == "J":
+            if scenario == 1:
+                return 11
+            if scenario == 2:
+                return 1
+        return alpha_card_values_scenario_one.get(card)
+    return int(card)
+
+def get_hand_type(hand: str, joker_is_wildcard: bool) -> HandType:
+    card_occurences_dict = get_card_occurences_in_hand(hand)
+    if joker_is_wildcard is True:
+        if card_occurences_dict.get("J") is not None:
+            most_common_card = { "value": "", "occurences": 0}
+            card_occurences_dict.pop("J")
+            for key in card_occurences_dict:
+                card_occurences = card_occurences_dict.get(key)
+                if card_occurences > most_common_card.get("occurences"):
+                    most_common_card.update({ "value": key, "occurences": card_occurences })
+            if most_common_card.get("occurences") == 0:
+                return get_hand_type(hand, False)
+            joker_replaced_hand = hand.replace("J", most_common_card.get("value"))
+            return get_hand_type(joker_replaced_hand, False)
 
     pair_count = 0
     has_trio = False
-    for key in occurences:
-        if occurences.get(key) == 5:
+    for key in card_occurences_dict:
+        if card_occurences_dict.get(key) == 5:
             return HandType.FIVE_OF_A_KIND
-        if occurences.get(key) == 4:
+        if card_occurences_dict.get(key) == 4:
             return HandType.FOUR_OF_A_KIND
-        if occurences.get(key) == 3:
+        if card_occurences_dict.get(key) == 3:
             has_trio = True
-        if occurences.get(key) == 2:
+        if card_occurences_dict.get(key) == 2:
             pair_count = pair_count + 1
     if has_trio:
         if pair_count > 0:
@@ -69,44 +91,33 @@ def get_hand_type(hand: str) -> HandType:
     
     return HandType.HIGH_CARD
 
-def compare_two_hands(hand_one: str, hand_two: str) -> int:
-    hand_one_type = get_hand_type(hand_one)
-    hand_two_type = get_hand_type(hand_two)
+def get_card_occurences_in_hand(hand: str):
+    split_hand = list(hand)
+    card_set = set(split_hand)
+    occurences = dict.fromkeys(card_set, 0)
+    for card_key in card_set:
+        card_occurences = 0
+        for card in split_hand:
+            if card == card_key:
+                card_occurences = card_occurences + 1
+        occurences.update({ card_key: card_occurences})
+    return occurences
+
+def compare_two_hands(hand_one: str, hand_two: str, scenario: int) -> int:
+    joker_is_wildcard = True if scenario == 2 else False
+    hand_one_type = get_hand_type(hand_one, joker_is_wildcard)
+    hand_two_type = get_hand_type(hand_two, joker_is_wildcard)
     if hand_one_type.value > hand_two_type.value:
         return 1
     if hand_one_type.value < hand_two_type.value:
         return -1
-    
+
     split_hand_one = list(hand_one)
     split_hand_two = list(hand_two)
     counter = 0
     while counter < len(split_hand_one):
-        card_one_value = split_hand_one[counter]
-        card_two_value = split_hand_two[counter]
-        if card_one_value == 'T':
-            card_one_value = 10
-        elif card_one_value == 'J':
-            card_one_value = 11
-        elif card_one_value == 'Q':
-            card_one_value = 12
-        elif card_one_value == 'K':
-            card_one_value = 13
-        elif card_one_value == 'A':
-            card_one_value = 14
-
-        if card_two_value == 'T':
-            card_two_value = 10
-        elif card_two_value == 'J':
-            card_two_value = 11
-        elif card_two_value == 'Q':
-            card_two_value = 12
-        elif card_two_value == 'K':
-            card_two_value = 13
-        elif card_two_value == 'A':
-            card_two_value = 14
-        card_one_value = int(card_one_value)
-        card_two_value = int(card_two_value)
-
+        card_one_value = get_alpha_card_values(split_hand_one[counter], scenario)
+        card_two_value = get_alpha_card_values(split_hand_two[counter], scenario)
         if card_one_value > card_two_value:
             return 1
         if card_one_value < card_two_value:
@@ -114,7 +125,7 @@ def compare_two_hands(hand_one: str, hand_two: str) -> int:
         counter = counter + 1
     return 0
 
-def quicksort_bids(card_bid_list: list[dict]):
+def quicksort_bids(card_bid_list: list[dict], scenario: int):
     list_length = len(card_bid_list)
     high_list = []
     low_list = []
@@ -123,11 +134,11 @@ def quicksort_bids(card_bid_list: list[dict]):
         return card_bid_list
     pivot = card_bid_list[0].get("hand")
     for card in card_bid_list:
-        hand_comparison = compare_two_hands(pivot, card.get("hand"))
+        hand_comparison = compare_two_hands(pivot, card.get("hand"), scenario)
         if hand_comparison == -1:
             high_list.append(card)
         elif hand_comparison == 1:
             low_list.append(card)
         else:
             equal_list.append(card)
-    return quicksort_bids(low_list) + equal_list + quicksort_bids(high_list)
+    return quicksort_bids(low_list, scenario) + equal_list + quicksort_bids(high_list, scenario)
--- 2023/tests/testDaySeven.py
+++ 2023/tests/testDaySeven.py
@@ -7,20 +7,32 @@ class TestDaySeven(unittest.TestCase):
         actual = seven(1, "./inputs/test/daySeven/one.txt").get("solution")
         self.assertEqual(actual, expected)
     
+    def test_day_seven_scenario_two(self):
+        expected = 5905
+        actual = seven(2, "./inputs/test/daySeven/one.txt").get("solution")
+        self.assertEqual(actual, expected)
+    
     def test_get_hand_type(self):
-        self.assertEqual(get_hand_type('12345'), HandType.HIGH_CARD)
-        self.assertEqual(get_hand_type('12234'), HandType.ONE_PAIR)
-        self.assertEqual(get_hand_type('12233'), HandType.TWO_PAIR)
-        self.assertEqual(get_hand_type('11123'), HandType.THREE_OF_A_KIND)
-        self.assertEqual(get_hand_type('11122'), HandType.FULL_HOUSE)
-        self.assertEqual(get_hand_type('11112'), HandType.FOUR_OF_A_KIND)
-        self.assertEqual(get_hand_type('11111'), HandType.FIVE_OF_A_KIND)
+        self.assertEqual(get_hand_type('12345', False), HandType.HIGH_CARD)
+        self.assertEqual(get_hand_type('12234', False), HandType.ONE_PAIR)
+        self.assertEqual(get_hand_type('12233', False), HandType.TWO_PAIR)
+        self.assertEqual(get_hand_type('11123', False), HandType.THREE_OF_A_KIND)
+        self.assertEqual(get_hand_type('11122', False), HandType.FULL_HOUSE)
+        self.assertEqual(get_hand_type('11112', False), HandType.FOUR_OF_A_KIND)
+        self.assertEqual(get_hand_type('11111', False), HandType.FIVE_OF_A_KIND)
+        self.assertEqual(get_hand_type('1234J', False), HandType.HIGH_CARD) 
+        self.assertEqual(get_hand_type('1234J', True), HandType.ONE_PAIR)
+        self.assertEqual(get_hand_type('11J22', True), HandType.FULL_HOUSE)
+        self.assertEqual(get_hand_type('JJJJJ', True), HandType.FIVE_OF_A_KIND)
     
     def test_compare_two_hands(self):
-        self.assertEqual(compare_two_hands('11111', '11111'), 0)
-        self.assertEqual(compare_two_hands('11111', '22222'), -1)
-        self.assertEqual(compare_two_hands('22222', '11111'), 1)
-        self.assertEqual(compare_two_hands('KQJ98', 'QJ891'), 1)
-        self.assertEqual(compare_two_hands('KQJ98', 'QKJ98'), 1)
-        self.assertEqual(compare_two_hands('KQJ98', '11111'), -1)
+        self.assertEqual(compare_two_hands('11111', '11111', 1), 0)
+        self.assertEqual(compare_two_hands('11111', '22222', 1), -1)
+        self.assertEqual(compare_two_hands('22222', '11111', 1), 1)
+        self.assertEqual(compare_two_hands('KQJ98', 'QJ891', 1), 1)
+        self.assertEqual(compare_two_hands('KQJ98', 'QKJ98', 1), 1)
+        self.assertEqual(compare_two_hands('KQJ98', '11111', 1), -1)
+        self.assertEqual(compare_two_hands('KKJKK', 'KKTKK', 1), 1)
+        self.assertEqual(compare_two_hands('2222J', '22229', 1), 1)
+        self.assertEqual(compare_two_hands('2222J', '22222', 2), -1)
 
